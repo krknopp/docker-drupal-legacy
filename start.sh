9e5426c0d553b804d65182ad034926bf764b2bbf
Kerry Knopp <kerry@codekoalas.com>
2016-08-05 09:32:05 -0500

    Get env vars in to cron

--- start.sh
+++ start.sh
@@ -3,6 +3,10 @@
 # Run Confd to make config files
 /usr/local/bin/confd -onetime -backend env
 
+# Export all env vars containing "_" to a file for use with cron jobs
+printenv | grep \_ | sed 's/^\(.*\)$/export \1/g' | sed 's/=/=\"/g' | sed 's/$/"/g' > /root/project_env.sh
+chmod +x /root/project_env.sh
+
 # Add gitlab to hosts file
 grep -q -F "$GIT_HOSTS" /etc/hosts  || echo $GIT_HOSTS >> /etc/hosts
 
