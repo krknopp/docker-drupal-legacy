992efa9eea016cb3ad077215ccf28b6d6d4adaea
Kerry Knopp <kerry@codekoalas.com>
2017-01-23 15:07:24 -0600

    fixing the package name for xdebug

--- start.sh
+++ start.sh
@@ -34,7 +34,7 @@ echo $(/usr/local/src/drush/drush --root=$APACHE_DOCROOT status | grep "Drupal v
 echo "[$(date +"%Y-%m-%d %H:%M:%S:%3N %Z")] NOTICE: Setting up XDebug based on state of LOCAL envvar"
 if [[ -n "$LOCAL" &&  $LOCAL = "true" ]] ; then
   /usr/bin/apt-get update && apt-get install -y \
-    php-xdebug \
+    php5-xdebug \
     --no-install-recommends && rm -r /var/lib/apt/lists/*
   cp /root/xdebug-php.ini /etc/php5/fpm/php.ini
   /usr/bin/supervisorctl restart php-fpm
