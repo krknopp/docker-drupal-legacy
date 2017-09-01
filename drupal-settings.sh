4041b5033c91d8c8d62cd1e02903b7f6717f2f80
Kerry Knopp <kerry@codekoalas.com>
2017-09-01 15:56:18 -0500

    adding script to remove blank base_url if env var not set

--- drupal-settings.sh
+++ drupal-settings.sh
@@ -5,3 +5,8 @@ if [ ! -h $APACHE_DOCROOT/sites/default/settings.php ]
   then
     /usr/local/bin/confd -onetime -backend env -confdir="/root/drupal7-settings"
 fi
+
+if [[ ! -n "$DRUPAL_BASE_URL" ]] ; then
+  chmod u+w -R /root/apache_docroot/sites/default
+  sed -i '/base_url/d' /root/apache_docroot/sites/default/settings.php
+fi
